16b5d000b7dea4b93de29305e1a1d641ffbde7b0
Tony Viola <maviola5@gmail.com>
2021-01-14 23:57:21 -0500

    feat: add dynamodb permissions

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -11,22 +11,21 @@ resource "aws_iam_policy" "lambda_logging" {
   path        = "/"
   description = "IAM policy for logging from a lambda"
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
+  policy = jsonencode(
     {
-      "Action": [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
-      "Resource": "arn:aws:logs:*:*:*",
-      "Effect": "Allow"
-    }
-  ]
-}
-EOF
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Action" : [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ],
+          "Resource" : "arn:aws:logs:*:*:*",
+          "Effect" : "Allow"
+        }
+      ]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
--- terraform/function.tf
+++ terraform/function.tf
@@ -1,21 +1,41 @@
 resource "aws_iam_role" "execution_role" {
   name = "${local.function_name}-execution-role"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : "sts:AssumeRole",
+        "Principal" : {
+          "Service" : "lambda.amazonaws.com"
+        },
+        "Effect" : "Allow",
+        "Sid" : ""
+      }
+    ]
+  })
 }
-EOF
+
+resource "aws_iam_role_policy" "role_policy" {
+  name = "${local.function_name}-role-policy"
+  role = aws_iam_role.execution_role.id
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [{
+      "Effect" : "Allow",
+      "Action" : [
+        "dynamodb:BatchGetItem",
+        "dynamodb:GetItem",
+        "dynamodb:Query",
+        "dynamodb:Scan",
+        "dynamodb:BatchWriteItem",
+        "dynamodb:PutItem",
+        "dynamodb:UpdateItem"
+      ],
+      "Resource" : "arn:aws:dynamodb:${local.region}:${data.aws_caller_identity.current.account_id}:table/ms-data"
+      }
+    ]
+  })
 }
 
 resource "aws_lambda_function" "function" {
