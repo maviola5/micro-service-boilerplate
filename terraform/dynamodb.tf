6c77c992288e654e9f2b9846f136eeb93ee701ed
Tony Viola <maviola5@gmail.com>
2021-01-14 23:37:48 -0500

    feat: remove claudia & enhance pack script

---
+++ terraform/dynamodb.tf
@@ -0,0 +1,16 @@
+resource "aws_dynamodb_table" "micro_service" {
+  name         = "ms-data"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+  range_key    = "timestamp"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "timestamp"
+    type = "S"
+  }
+}
